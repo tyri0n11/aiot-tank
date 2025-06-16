d8d6a084fd5972163a1e41d814c7f97170e28d49
tyr1on <chauthinh00710@gmail.com>
2025-06-16 21:20:22 +0700

    config mqtt connection

---
+++ hardware/main/main.ino
@@ -0,0 +1,65 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>  
+#include <PubSubClient.h>
+
+// WiFi
+const char *ssid = SSID; // Enter your Wi-Fi name
+const char *password = WIFI_PASSWORD;  // Enter Wi-Fi password
+
+// MQTT Broker
+const char *mqtt_broker = MQTT_BROKER;
+const char *topic = MQTT_TOPIC;
+const char *mqtt_username = MQTT_USERNAME;
+const char *mqtt_password = MQTT_PASSWORD;
+const int mqtt_port = MQTT_PORT;
+
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+
+void setup() {
+    // Set software serial baud to 115200;
+    Serial.begin(115200);
+    // Connecting to a WiFi network
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Connecting to WiFi..");
+    }
+    Serial.println("Connected to the Wi-Fi network");
+    //connecting to a mqtt broker
+    //test/dev
+    espClient.setInsecure();
+    client.setServer(mqtt_broker, mqtt_port);
+    client.setCallback(callback);
+    while (!client.connected()) {
+        String client_id = "esp32-client-";
+        client_id += String(WiFi.macAddress());
+        Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
+        if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+            Serial.println("Public EMQX MQTT broker connected");
+        } else {
+            Serial.print("failed with state ");
+            Serial.print(client.state());
+            delay(2000);
+        }
+    }
+    // Publish and subscribe
+    client.publish(topic, "Hi, I'm ESP32 ^^");
+    Serial.println("published message");
+    client.subscribe(topic);
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+    Serial.print("Message arrived in topic: ");
+    Serial.println(topic);
+    Serial.print("Message:");
+    for (int i = 0; i < length; i++) {
+        Serial.print((char) payload[i]);
+    }
+    Serial.println();
+    Serial.println("-----------------------");
+}
+
+void loop() {
+    client.loop();
+}
